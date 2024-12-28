f3717743fca9c7ef4c0cd4f4a49f68349cbb9b8a
kurtlowe <109322805+kurtlowe@users.noreply.github.com>
2024-12-28 14:37:58 +1000

    first commit

---
+++ ConsoleProject/DialogWelcome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    internal class DialogWelcome
+    {
+        public void DialogWelcomeText() 
+        {
+            Console.Clear();
+
+            Console.WriteLine("Pleas Enter Your Player Name:  ");
+            string PlayerName = Console.ReadLine();
+
+            Console.WriteLine("Welcome: " + PlayerName);
+        }
+    }
+}
---
+++ ConsoleProject/Map1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleProject
+{
+    internal class Map1
+    {
+        public void Map1Generation () 
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
